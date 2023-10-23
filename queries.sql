dc4395fcbdda0bffb15e8f7e0212b7ea9a4710ec
DropDeadoo <68104221+DropDeadoo@users.noreply.github.com>
2023-10-23 13:52:18 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -72,3 +72,56 @@ with total_sales as (
 /* Достаем из таблицы выше только имя продавца, день недели и сумму продаж */		
 select name, weekday, income
 from total_sales;
+
+/* Запрос делит покупателей по категориям и считает их кол-во.
+ * age_category - возрастная группа
+ * count - количество человек в группе
+ */
+
+select case when age >= 16 and age <= 25 then '16-25' when age >= 26 and age <= 40 then '26-40' else '40+' end as age_category,
+count(age)
+from customers 
+group by 1
+order by 1
+;
+/* Запрос находит количество уникальных пользователей и выручку в разрезе даты. 
+ * date - дата в указанном формате
+ * total_customers - количество покупателей
+ * income - принесенная выручка
+*/
+/* Создаем вспомогательную таблицу с датой формата ГОД-МЕСЯЦ, кол-вом покупателей и выручкой в разрезе дат*/
+with income as (
+				select to_char(s.sale_date, 'YYYY-MM') as date,
+				count(s.*) as total_customers,
+				s.quantity * p.price as income
+				from sales s
+				join products p 
+				on s.product_id = p.product_id 
+				group by 1, 3
+				order by 1
+				)
+/* Достаем из таблицы выше информацию и суммируем полученные значения в разрезе каждого месяца и года.*/
+select date, sum(total_customers) as total_customers,
+round(sum(income), 0) as income
+from income
+group by 1
+order by 1;
+
+/* Запрос выводит покупателей, ПЕРВАЯ покупка которых была в ходе проведения акций (товар = 0). 
+ * customer - имя и фамилия покупателя
+ * sale_date - дата покупки
+ * seller - имя и фамилия продавца
+*/
+select concat(c.first_name, ' ', c.last_name) as customer, 
+min(s.sale_date) as date,
+concat(e.first_name, ' ', e.last_name) as seller
+from sales s
+join products p
+on s.product_id = p.product_id
+join customers c
+on s.customer_id = c.customer_id
+join employees e
+on s.sales_person_id = e.employee_id
+where p.price = 0
+group by 1, 3
+order by 1;
